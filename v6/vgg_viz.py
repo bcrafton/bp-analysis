0d3963888ebdd4b96e21bbacf20148b9833c3fbe
Brian Crafton <crafton.b@husky.neu.edu>
2019-06-06 23:23:21 -0400

    great viz

---
+++ v6/vgg_viz.py
@@ -0,0 +1,68 @@
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from conv import conv
+from combine_filter import combine_filter
+
+#####
+
+img = np.random.uniform(low=0., high=1., size=(1, 64, 64, 3))
+
+#####
+
+lel = 1
+
+if lel:
+    f1 = np.load('vgg64_lel.npy').item()['conv1']
+    f2 = np.load('vgg64_lel.npy').item()['conv2']
+    f3 = np.load('vgg64_lel.npy').item()['conv3']
+    f4 = np.load('vgg64_lel.npy').item()['conv4']
+    f5 = np.load('vgg64_lel.npy').item()['conv5']
+    f6 = np.load('vgg64_lel.npy').item()['conv6']
+    f7 = np.load('vgg64_lel.npy').item()['conv7']
+    f8 = np.load('vgg64_lel.npy').item()['conv8']
+else:
+    f1 = np.load('vgg64_bp.npy').item()['conv1']
+    f2 = np.load('vgg64_bp.npy').item()['conv2']
+    f3 = np.load('vgg64_bp.npy').item()['conv3']
+    f4 = np.load('vgg64_bp.npy').item()['conv4']
+    f5 = np.load('vgg64_bp.npy').item()['conv5']
+    f6 = np.load('vgg64_bp.npy').item()['conv6']
+    f7 = np.load('vgg64_bp.npy').item()['conv7']
+    f8 = np.load('vgg64_bp.npy').item()['conv8']
+
+#####
+
+f12       = combine_filter(f1,       f2, stride=1); print (np.shape(f12))
+f123      = combine_filter(f12,      f3, stride=2); print (np.shape(f123))
+f1234     = combine_filter(f123,     f4, stride=2); print (np.shape(f1234))
+f12345    = combine_filter(f1234,    f5, stride=4); print (np.shape(f12345))
+f123456   = combine_filter(f12345,   f6, stride=4); print (np.shape(f123456))
+f1234567  = combine_filter(f123456,  f7, stride=8); print (np.shape(f1234567))
+f12345678 = combine_filter(f1234567, f8, stride=8); print (np.shape(f12345678))
+
+#####
+
+filters = f12345678
+
+filters = filters / np.max(filters)
+_, _, fin, fout = np.shape(filters)
+
+'''
+for ii in range(fin):
+    for jj in range(fout):
+        print (ii, jj)
+        plt.imsave('./imgs/%d_%d.jpg' % (ii, jj), filters[:, :, ii, jj])
+'''
+
+for ii in range(fout):
+    print (ii)
+    plt.imsave('./imgs/%d.jpg' % (ii), filters[:, :, :, ii])
+
+
+
+
+
+
+
