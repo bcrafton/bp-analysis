e01e44f7265447961deaaf355517c8bcf36099bf
Brian Crafton <crafton.b@husky.neu.edu>
2019-06-07 16:41:08 -0400

    viz 3 filter

--- v6/viz_filter.py
+++ v6/viz_filter.py
@@ -36,3 +36,25 @@ def viz_filter(name, filters):
             
     plt.imsave(name, img, cmap="gray")
 
+def viz_filter_3_channels(name, filters):
+    fh, fw, fin, fout = np.shape(filters)
+    filters = np.transpose(filters, (3, 0, 1, 2))
+    assert(fin == 3)
+    [nrows, ncols] = factors(fout)
+    filters = np.reshape(filters, (nrows, ncols, fw, fh, fin))
+
+    for ii in range(nrows):
+        for jj in range(ncols):
+            if jj == 0:
+                row = filters[ii][jj]
+            else:
+                row = np.concatenate((row, filters[ii][jj]), axis=1)
+                
+        if ii == 0:
+            img = row
+        else:
+            img = np.concatenate((img, row), axis=0)
+            
+    plt.imsave(name, img)
+    
+    
